3161fb2427f3dbb68dd6fff103f9238108c03a9c
Atropa-Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2025-02-27 10:15:32 -0500

    fix bug

--- utils/gallery_generator.py
+++ utils/gallery_generator.py
@@ -4,6 +4,15 @@ import json
 # generates galleries.json file for each subdirectory to add galleries
 def generate_galleries_json(directory):
     galleries = []
+    existing_galleries = {}
+
+    # Load existing galleries.json if it exists
+    galleries_json_path = os.path.join(directory, 'galleries.json')
+    if os.path.exists(galleries_json_path):
+        with open(galleries_json_path, 'r') as existing_file:
+            existing_data = json.load(existing_file)
+            existing_galleries = {gallery['name']: gallery for gallery in existing_data.get('galleries', [])}
+
     for root, dirs, files in os.walk(directory):
         for folder in dirs:
             folder_path = os.path.join(root, folder)
@@ -11,7 +20,7 @@ def generate_galleries_json(directory):
             if os.path.exists(manifest_path):
                 with open(manifest_path, 'r') as manifest_file:
                     images = json.load(manifest_file)
-                    cover_photo = images[0] if images else ''
+                    cover_photo = existing_galleries.get(folder, {}).get('coverPhoto', images[0] if images else '')
                     galleries.append({
                         "name": os.path.relpath(folder_path, directory).replace(os.sep, '-'),
                         "title": folder.replace('-', ' ').title(),
@@ -21,9 +30,9 @@ def generate_galleries_json(directory):
     galleries_json = {
         "galleries": galleries
     }
-    with open(os.path.join(directory, 'galleries.json'), 'w') as json_file:
+    with open(galleries_json_path, 'w') as json_file:
         json.dump(galleries_json, json_file, indent=4)
-    print(f"Generated {os.path.join(directory, 'galleries.json')}")
+    print(f"Generated {galleries_json_path}")
 
 if __name__ == "__main__":
     images_directory = os.path.join('public', 'images')
