926ffa2c85adbeefacd054ad15c0e8cb0a77e891
Solanaceae <89823371+0xSolanaceae@users.noreply.github.com>
2025-03-24 00:44:31 -0400

    Patrica & Joell

--- utils/handler.py
+++ utils/handler.py
@@ -1,19 +1,15 @@
 import subprocess
+import os
 
 def run_script(script_path):
-    try:
-        subprocess.run(["python", script_path], check=True)
-        print(f"{script_path} ran successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running {script_path}:\n{e}")
-
-def git_commit_changes():
-    try:
-        subprocess.run(["git", "add", "."], check=True)
-        subprocess.run(["git", "commit", "-m", "Automated commit of changes made by handler.py"], check=True)
-        print("Changes committed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error committing changes:\n{e}")
+    if os.path.exists(script_path):
+        try:
+            subprocess.run(["python", script_path], check=True)
+            print(f"{script_path} ran successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running {script_path}:\n{e}")
+    else:
+        print(f"{script_path} not found.")
 
 if __name__ == "__main__":
     idiot_check = input("Please confirm you have uploaded the full resolution gallery to the file hosting site (github) prior to running this script. \nYour images are about to be irreversably compressed for website hosting. (yes/no): ")
@@ -21,13 +17,11 @@ if __name__ == "__main__":
         print("Make good decisions.")
         exit()
     scripts = [
-        'img_resizer.py',
-        'gallery_generator.py',
-        'manifest_generator.py',
-        'secrets_setter.py'
+        'utils/img_resizer.py',
+        'utils/gallery_generator.py',
+        'utils/manifest_generator.py',
+        'utils/secrets_setter.py'
     ]
 
     for script in scripts:
         run_script(script)
-
-    git_commit_changes()
